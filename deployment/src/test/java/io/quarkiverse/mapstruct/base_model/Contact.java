6946e3d7b72089843dae8cb594bbc59f3f12b1ef
Martin Panzer <postremus1996@googlemail.com>
2025-12-01 19:46:49 +0100

    Automatically recompile Mapper classes on model change

---
+++ deployment/src/test/java/io/quarkiverse/mapstruct/base_model/Contact.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.mapstruct.base_model;
+
+public class Contact extends ModelBase {
+    private String name;
+
+    private Email email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+}
